ab0112086ea8c7895bbcf1904ad149553fe39887
manav-1 <manav81101@gmail.com>
2022-01-05 15:55:41 +0530

    Changes in Readme And index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { Router } from "@reach/router";
-import Login from "../components/Login";
-import PrivateRoute from "../components/PrivateRoute";
-import Signup from "../components/Signup";
-import Home from "../components/Home";
+import { Link } from "gatsby";
+import Layout from "../components/Layout";
+import View from "../components/View";
+import Status from "../components/Status";
 
-const App = () => (
-  <Router>
-    <Login default path="/app/login" />
-    <PrivateRoute path="/app/home" component={Home} />
-    
-    <Signup path="/app/signup" />
-  </Router>
+const Index = () => (
+  <View title="Simple Authentication Example">
+    <p>
+      This is a simple example of creating dynamic apps with Gatsby that require
+      user authentication. It uses concepts from the
+      <a href="https://www.gatsbyjs.com/docs/client-only-routes-and-user-authentication/">
+        client-only routes section
+      </a>
+      of the “Building Apps with Gatsby” documentation.
+    </p>
+    <p>
+      For the full experience, go to
+      <Link to="/app/home">your home</Link>.
+    </p>
+  </View>
 );
 
-export default App;
+export default Index;
